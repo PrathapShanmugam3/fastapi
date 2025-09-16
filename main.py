3b73231405baa3ac1c3cd9abe580a76f4a044e46
prathapshanmugam3 <prathapshanmugam5@gmail.com>
2025-09-16 15:19:24 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from app.api.v1.endpoints import user
+
+app = FastAPI(title="FastAPI Real-Time Project")
+
+app.include_router(user.router, prefix="/api/v1/user", tags=["Users"])
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, FastAPI!"}
+
