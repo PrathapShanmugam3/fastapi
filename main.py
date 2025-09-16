dff3a8100c8a83f663aeec5c97a5d9554175450a
prathapshanmugam3 <prathapshanmugam5@gmail.com>
2025-09-16 17:47:12 +0530

    db changes

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
-from api.v1.endpoints import user
+from db import engine, Base
+from controller import user_controller
+from controller.user_controller import router as user_router
+from sqlalchemy import text 
 
 app = FastAPI(title="FastAPI Real-Time Project")
+Base.metadata.create_all(bind=engine)  
 
-app.include_router(user.router, prefix="/api/v1/user", tags=["Users"])
+@app.on_event("startup")
+async def startup_event():
+    try:
+        with engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+        print("✅ Database connected successfully!")
+    except Exception as e:
+        print(f"❌ Database connection failed: {e}")
 
 @app.get("/")
 async def root():
     return {"message": "Hello, FastAPI!"}
+
+app.include_router(user_router)
+
+
 
