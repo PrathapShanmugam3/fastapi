8b10368ba07dee11f63d097250b048846e7b2f51
prathapshanmugam3 <prathapshanmugam5@gmail.com>
2025-09-16 17:05:36 +0530

    db config

--- api/v1/endpoints/user.py
+++ api/v1/endpoints/user.py
@@ -1,7 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from db import get_db
+import crud
 
 router = APIRouter()
 
 @router.get("/")
-def get_users():
-    return [{"id": 1, "name": "Prathap"}]
+def list_users(db: Session = Depends(get_db)):
+    return crud.get_users(db)
+
+@router.post("/")
+def add_user(name: str, email: str, db: Session = Depends(get_db)):
+    return crud.create_user(db, name, email)
+
---
+++ core/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # will load .env locally
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+
+DATABASE_URL = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
